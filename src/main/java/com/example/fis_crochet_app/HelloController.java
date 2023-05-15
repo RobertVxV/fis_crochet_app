3140797bea60b6fa03010509a59f239d35d15603
RobertVxV <robertvinitchi@gmail.com>
2023-05-15 22:03:19 +0300

    create login page layout, needs database connection and functionality

--- src/main/java/com/example/fis_crochet_app/HelloController.java
+++ src/main/java/com/example/fis_crochet_app/HelloController.java
@@ -1,14 +1,36 @@
 package com.example.fis_crochet_app;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Label loginMessageLabel;
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private PasswordField passwordField;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+    public void onLoginButtonClick(ActionEvent e){
+
+
+        if(usernameTextField.getText().isBlank() == false && passwordField.getText().isBlank() == false) {
+            loginMessageLabel.setText("You tried to login.");
+        }
+        else {
+            loginMessageLabel.setText("Please enter username and password.");
+        }
     }
 }
