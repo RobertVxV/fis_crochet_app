f5fd74b27c270fb161799a8dde0848941f4086cf
Robert Vinițchi <robertvinitchi@gmail.com>
2023-05-17 10:29:05 +0300

    CA-1 Login Page - Changed file names.

--- src/main/java/com/example/fis_crochet_app/LoginController.java
+++ src/main/java/com/example/fis_crochet_app/LoginController.java
@@ -7,7 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
-public class HelloController {
+public class LoginController {
     @FXML
     private Label welcomeText;
     @FXML
--- src/main/java/com/example/fis_crochet_app/LoginPage.java
+++ src/main/java/com/example/fis_crochet_app/LoginPage.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class LoginPage extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginPage.class.getResource("login_page_layout.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Crochet Application");
         stage.setScene(scene);
