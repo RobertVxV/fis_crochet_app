ea7eeed16a49ba50af47f1c3f988a404660e3f90
RobertVxV <robertvinitchi@gmail.com>
2023-05-20 14:40:21 +0300

    Commit summary: - Set gradle main page as: LoginPage - Created layout from both register and login pages. - Linked register and login pages using buttons. - Created some simple user input verifications.
    To do:
    - Link to main page after successfully logging in.
    - Load users information to app.
    - Add users to database using register form.
    - User login verification from database.

--- src/main/java/com/example/fis_crochet_app/LoginController.java
+++ src/main/java/com/example/fis_crochet_app/LoginController.java
@@ -1,13 +1,27 @@
 package com.example.fis_crochet_app;
 
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+
+import com.example.fis_crochet_app.LoginPage.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
     @FXML
     private Label welcomeText;
     @FXML
@@ -18,19 +32,35 @@ public class LoginController {
     private TextField usernameTextField;
     @FXML
     private PasswordField passwordField;
+    @FXML
+    private Button registerButton;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+    @FXML
     public void onLoginButtonClick(ActionEvent e){
 
 
         if(usernameTextField.getText().isBlank() == false && passwordField.getText().isBlank() == false) {
+            loginMessageLabel.setTextFill(Color.GREEN);
             loginMessageLabel.setText("You tried to login.");
         }
         else {
             loginMessageLabel.setText("Please enter username and password.");
         }
+    }
+    @FXML
+    public void openRegisterPage(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("register_page_layout.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    public void onRegisterButtonClick(ActionEvent event) throws IOException {
+        openRegisterPage(event);
     }
 }
--- src/main/java/com/example/fis_crochet_app/RegisterPage.java
+++ src/main/java/com/example/fis_crochet_app/RegisterPage.java
@@ -6,24 +6,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-import com.example.fis_crochet_app.LoginController.*;
+import com.example.fis_crochet_app.RegisterController;
 
-public class LoginPage extends Application {
+public class RegisterPage extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        //stage.setResizable(false);
-        FXMLLoader fxmlLoader = new FXMLLoader(LoginPage.class.getResource("login_page_layout.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginPage.class.getResource("register_page_layout.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 650, 450);
         stage.setTitle("Crochet Application");
         stage.setScene(scene);
         stage.show();
     }
-
     public static void main(String[] args) {
         launch();
     }
